eac2120600ddc50c375879f3a84976dea7bc5053
Abhimanyu Gangwar <ag15081993@gmail.com>
2020-10-30 01:36:52 +0000

    Fills contact page except the country code

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,75 @@
-import { printLine } from './modules/print';
-
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+function getLabelForId(label, index) {
+  const labelElements = $('label:contains("' + label + '")');
+  const labelElement = index ? $(labelElements[index]) : labelElements.first();
+  return (
+    '#' +
+    labelElement
+      .attr('for')
+      .replace(/\./g, '\\.')
+      .replace(/\[/g, '\\[')
+      .replace(/\]/g, '\\]')
+  );
+}
+
+const contactInputs = [
+  {
+    label: 'Country',
+    value: 'Ireland',
+  },
+  {
+    label: 'Given Name',
+    value: 'Lulu',
+    index: 3,
+  },
+  {
+    label: 'Family Name',
+    value: 'Niu niu',
+    index: 3,
+  },
+  {
+    label: 'Address Line 1',
+    value: '10 Linnetfields Park, Castaheany, Clonee',
+  },
+  {
+    label: 'Postal Code',
+    value: 'D15 N6F5',
+  },
+  {
+    label: 'City',
+    value: 'Dublin',
+  },
+  {
+    label: 'Phone Number',
+    value: '873444925',
+  },
+];
+
+function fillContact(label, value, index) {
+  switch (label) {
+    case 'Country':
+      $(getLabelForId(label)).children().first().children().text(value);
+      break;
+    default:
+      $(getLabelForId(label, index)).val(value);
+      break;
+  }
+}
+
+function fillContactForm() {
+  contactInputs.forEach((contactInput) =>
+    fillContact(contactInput.label, contactInput.value, contactInput.index)
+  );
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension'
+  );
+  if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+  fillContactForm();
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -4,21 +4,22 @@ import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
+  function fillContactPage() {
+    console.log('Trying to fill contact page');
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { greeting: 'hello' }, function (
+        response
+      ) {
+        console.log(response.farewell);
+      });
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="primary" onClick={fillContactPage}>
+          Fill contact page
+        </button>
       </header>
     </div>
   );
